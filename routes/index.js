8e6847f0ea4617e0e406311deb6c70af639779f4
hatimb03 <hatim.ba53@gmail.com>
2024-08-09 18:50:33 +0530

    total cart price feature added

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,26 @@ router.get("/", function (req, res) {
 
 router.get("/shop", isLoggedIn, async function (req, res) {
   let allProducts = await productModel.find();
+  let user = await userModel.findOne({ email: req.user.email });
   let success = req.flash("success");
-  res.render("shop", { allProducts, success });
+  res.render("shop", { allProducts, user, success });
 });
 
-router.get("/cart", isLoggedIn, function (req, res) {
-  res.render("cart");
+router.get("/cart", isLoggedIn, async function (req, res) {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+  let sum = 0;
+  let discount = 0;
+  let mrp = 0;
+  user.cart.forEach((val) => {
+    sum = sum + (val.price - val.discount);
+    discount = discount + val.discount;
+    mrp = mrp + val.price;
+  });
+  const breakdown = { sum, discount, mrp };
+
+  res.render("cart", { user, breakdown });
 });
 
 router.get("/addtocart/:productId", isLoggedIn, async function (req, res) {
